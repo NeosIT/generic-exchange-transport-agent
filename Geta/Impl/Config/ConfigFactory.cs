6dbd78cf5b8068da73fbe764541056fa7c668f94
Meikel Philipp <MeikelLP@users.noreply.github.com>
2019-01-15 08:41:12 +0100

    Core * Implemented hot reload for the configuration.

--- Geta/Impl/Config/ConfigFactory.cs
+++ Geta/Impl/Config/ConfigFactory.cs
@@ -8,56 +8,52 @@ namespace NeosIT.Exchange.GenericExchangeTransportAgent.Impl.Config
 {
     public static class ConfigFactory
     {
-        private static readonly string ConfigFileName =
-            Path.Combine(Path.GetDirectoryName(Assembly.GetAssembly(typeof (TransportAgentConfig)).Location),
-                         "config.xml");
+        public const string ConfigFileName = "config.xml";
+
+        public static string ConfigPath =>
+            Path.Combine(
+                Path.GetDirectoryName(Assembly.GetAssembly(typeof(TransportAgentConfig)).Location) ?? "",
+                ConfigFileName
+            );
 
         private static TransportAgentConfig _config;
+        
+        // ReSharper disable once PrivateFieldCanBeConvertedToLocalVariable
+        private static readonly FileSystemWatcher FileSystemWatcher;
 
-        private static long _configFileSize;
-        private static byte[] _configFileHash;
+        static ConfigFactory()
+        {
+            FileSystemWatcher = new FileSystemWatcher
+            {
+                Path = Path.GetDirectoryName(ConfigPath),
+                Filter = ConfigFileName,
+                NotifyFilter = NotifyFilters.LastWrite,
+                EnableRaisingEvents = true,
+                IncludeSubdirectories = false
+            };
+            FileSystemWatcher.Changed += FileSystemWatcherOnChanged;
+        }
+
+        private static void FileSystemWatcherOnChanged(object sender, FileSystemEventArgs e)
+        {
+            _config = LoadConfiguration();
+        }
 
         public static TransportAgentConfig GetConfig()
         {
-            var fileInfo = new FileInfo(ConfigFileName);
-            if (fileInfo.Length != _configFileSize)
-            {
-                LoadConfiguration();
-            }
-            else
-            {
-                var computedHash = ComputeConfigHash();
-                if (computedHash != _configFileHash)
-                {
-                    LoadConfiguration();
-                }
-            }
-
-            return _config;
+            return _config ?? (_config = LoadConfiguration());
         }
 
-        private static byte[] ComputeConfigHash()
+        private static TransportAgentConfig LoadConfiguration()
         {
-            var hash = HashAlgorithm.Create();
-            using (var fileStream = new FileStream(ConfigFileName, FileMode.Open, FileAccess.Read))
-            {
-                return hash.ComputeHash(fileStream);
-            }
-        }
-
-        private static void LoadConfiguration()
-        {
-            string path = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            var path = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
 
             IPluginHost pluginHost = new PluginHost(path);
-            var fileInfo = new FileInfo(ConfigFileName);
-            _configFileSize = fileInfo.Length;
-            _configFileHash = ComputeConfigHash();
             var settings = new XmlReaderSettings {ConformanceLevel = ConformanceLevel.Auto,};
-            var serializer = new DataContractSerializer(typeof (TransportAgentConfig), pluginHost.KnownTypes);
-            using (var reader = XmlReader.Create(ConfigFileName, settings))
+            var serializer = new DataContractSerializer(typeof(TransportAgentConfig), pluginHost.KnownTypes);
+            using (var reader = XmlReader.Create(ConfigPath, settings))
             {
-                _config = (TransportAgentConfig) serializer.ReadObject(reader, true);
+                return (TransportAgentConfig) serializer.ReadObject(reader, true);
             }
         }
     }

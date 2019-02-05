91435b646694bec601ba0cb87c71ebaf10990804
Christoper Klein <ckl@neos-it.de>
2019-02-05 14:03:32 +0100

    [changed] "-property" to "/p:" to prevent escaping/quoting issues with msbuild

--- build.ps1
+++ build.ps1
@@ -59,7 +59,7 @@ if ($LastExitCode -gt 0) {
 }
 
 # clean and build with msbuild
-$msbuildArgs = @("-nologo", "-maxcpucount", "-verbosity:$logLevel", "-property:Configuration=""$BuildTarget""", "-property:ExchangeLibraryPath=""$useLibraryPath""")
+$msbuildArgs = @("-nologo", "-maxcpucount", "-verbosity:$logLevel", "/p:Configuration=""$BuildTarget""", "/p:ExchangeLibraryPath=""$useLibraryPath""")
 Write-Host "Running msbuild with arguments: $msbuildArgs"
 & $msbuildExe $msbuildArgs "/t:clean" $SlnPath
 

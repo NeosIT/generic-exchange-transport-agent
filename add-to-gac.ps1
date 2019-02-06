760c07945e27a7ff389435255fa6808f3b35eb35
Meikel Philipp <MeikelLP@users.noreply.github.com>
2019-02-06 13:36:33 +0100

    GAC script now more flexible

--- add-to-gac.ps1
+++ add-to-gac.ps1
@@ -1,11 +1,12 @@
 param(
   # Directory in which the dll files shall be added to the gac
-  [String][Parameter(Mandatory=$true)]$Path
+  [String][Parameter(Mandatory=$true)]$Path,
+  [string]$GacUtilPath="C:\Program Files (x86)\Microsoft SDKs\Windows\v10.0A\bin\NETFX 4.7.2 Tools\"
 )
-$gacutilPath = "C:\Program Files (x86)\Microsoft SDKs\Windows\v10.0A\bin\NETFX 4.7.2 Tools\gacutil.exe"
+$gacUtilPath = "$GacUtilPath\gacutil.exe"
 $files = Get-ChildItem -Path $Path -Filter "*.dll" | Select-Object -ExpandProperty FullName
 
 foreach($file in $files){
   Write-Host "Adding $file"
-  & $gacutilPath -nologo -if $file
+  & $gacUtilPath -nologo -if $file
 }
